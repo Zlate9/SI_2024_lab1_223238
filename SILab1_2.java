108b77832c6653be20dcdc5ff11c68bbe8b8db2a
Zlate9 <zlatkojovanovski12345@gmail.com>
2024-04-14 21:09:36 +0200

    Fix wrong condition in makeBigStringFrom

--- SILab1_2.java
+++ SILab1_2.java
@@ -1,1 +1,14 @@
-400: Invalid request
+import java.util.List;
+
+public class SILab1_2 {
+
+    private static String makeBigStringFrom (List<String> list, int minLength) {
+        StringBuilder sb = new StringBuilder();
+        for (String el : list) {
+            if (el.length() >= minLength) {
+                sb.append(el);
+            }
+        }
+        return sb.toString();
+    }
+}
